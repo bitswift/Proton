50e09431c58ade7fd9f954be653a0e8a2b76a111
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-09 17:34:25 -0800

    Removed stub framework and test files autogenerated by Xcode

--- Frameworks/Proton/Proton/Proton.h
+++ Frameworks/Proton/Proton/Proton.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2011 Emerald Lark. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
-@interface Proton : NSObject
-
-@end
+#import <Proton/EXTRuntimeExtensions.h>
+#import <Proton/EXTSafeCategory.h>
+#import <Proton/EXTScope.h>
