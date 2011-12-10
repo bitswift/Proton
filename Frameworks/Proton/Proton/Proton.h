7151a4711a413df1a9d4af7094e9bb0555298392
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-09 16:24:35 -0800

    Set up Proton Mac OS X framework project

---
+++ Frameworks/Proton/Proton/Proton.h
@@ -0,0 +1,13 @@
+//
+//  Proton.h
+//  Proton
+//
+//  Created by Justin Spahr-Summers on 09.12.11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Proton : NSObject
+
+@end
---
+++ Frameworks/Proton/Proton/Proton.m
@@ -0,0 +1,23 @@
+//
+//  Proton.m
+//  Proton
+//
+//  Created by Justin Spahr-Summers on 09.12.11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import "Proton.h"
+
+@implementation Proton
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
