79287584d918f24bd151931281636936d87c89af
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-13 11:54:19 -0800

    Added EXTNil to Proton, for representing nil values as objects (and not throwing exceptions like NSNull does)

--- Frameworks/Proton/Proton/Proton.h
+++ Frameworks/Proton/Proton/Proton.h
@@ -6,6 +6,7 @@
 //  Copyright (c) 2011 Emerald Lark. All rights reserved.
 //
 
+#import <Proton/EXTNil.h>
 #import <Proton/EXTRuntimeExtensions.h>
 #import <Proton/EXTSafeCategory.h>
 #import <Proton/EXTScope.h>
