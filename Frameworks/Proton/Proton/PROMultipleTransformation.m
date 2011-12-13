99d4f600bdce79b119c1e8f525971153c51f6583
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-13 01:35:16 -0800

    Stubbed out implementation of PROMultipleTransformation

--- Frameworks/Proton/Proton/PROMultipleTransformation.m
+++ Frameworks/Proton/Proton/PROMultipleTransformation.m
@@ -10,4 +10,49 @@
 
 @implementation PROMultipleTransformation
 
+#pragma mark Properties
+
+@synthesize transformations = m_transformations;
+
+#pragma mark Lifecycle
+
+- (id)init; {
+    return [self initWithTransformations:nil];
+}
+
+- (id)initWithTransformations:(NSArray *)transformations; {
+    // TODO
+    return nil;
+}
+
+#pragma mark Transformation
+
+- (id)transform:(id)obj; {
+    // TODO
+    return obj;
+}
+
+- (PROTransformation *)reverseTransformation; {
+    // TODO
+    return self;
+}
+
+#pragma mark NSCoding
+
+- (id)initWithCoder:(NSCoder *)coder {
+    // TODO
+    return nil;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+    // TODO
+}
+
+#pragma mark NSCopying
+
+- (id)copyWithZone:(NSZone *)zone {
+    // this object is immutable
+    return self;
+}
+
 @end
