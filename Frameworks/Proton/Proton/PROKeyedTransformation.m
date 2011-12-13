0f88f6d48afe392bc786e6cb1e18a4ec53d9c441
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-13 01:35:56 -0800

    Stubbed out implementation of PROKeyedTransformation

--- Frameworks/Proton/Proton/PROKeyedTransformation.m
+++ Frameworks/Proton/Proton/PROKeyedTransformation.m
@@ -10,4 +10,49 @@
 
 @implementation PROKeyedTransformation
 
+#pragma mark Properties
+
+@synthesize valueTransformations = m_valueTransformations;
+
+#pragma mark Lifecycle
+
+- (id)init; {
+    return [self initWithValueTransformations:nil];
+}
+
+- (id)initWithValueTransformations:(NSDictionary *)valueTransformations; {
+    // TODO
+    return nil;
+}
+
+#pragma mark Transformation
+
+- (id)transform:(id)obj; {
+    // TODO
+    return obj;
+}
+
+- (PROTransformation *)reverseTransformation; {
+    // TODO
+    return self;
+}
+
+#pragma mark NSCoding
+
+- (id)initWithCoder:(NSCoder *)coder {
+    // TODO
+    return nil;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+    // TODO
+}
+
+#pragma mark NSCopying
+
+- (id)copyWithZone:(NSZone *)zone {
+    // this object is immutable
+    return self;
+}
+
 @end
