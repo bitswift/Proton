0e1a4120838d08dd4ae0a5517f2bfb9ad22efc7e
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-13 01:32:38 -0800

    Stubbed out implementation of PROTransformation

--- Frameworks/Proton/Proton/PROTransformation.m
+++ Frameworks/Proton/Proton/PROTransformation.m
@@ -10,4 +10,34 @@
 
 @implementation PROTransformation
 
+#pragma mark Transformation
+
+- (id)transform:(id)obj; {
+    // TODO
+    return obj;
+}
+
+- (PROTransformation *)reverseTransformation; {
+    // TODO
+    return self;
+}
+
+#pragma mark NSCoding
+
+- (id)initWithCoder:(NSCoder *)coder {
+    // TODO
+    return nil;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+    // TODO
+}
+
+#pragma mark NSCopying
+
+- (id)copyWithZone:(NSZone *)zone {
+    // this object is immutable
+    return self;
+}
+
 @end
