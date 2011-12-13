5ea74d15cbad0b052c383f0b26085b202de56fb3
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-13 01:33:47 -0800

    Stubbed out implementation of PROUniqueTransformation

--- Frameworks/Proton/Proton/PROUniqueTransformation.m
+++ Frameworks/Proton/Proton/PROUniqueTransformation.m
@@ -10,4 +10,50 @@
 
 @implementation PROUniqueTransformation
 
+#pragma mark Properties
+
+@synthesize inputValue = m_inputValue;
+@synthesize outputValue = m_outputValue;
+
+#pragma mark Lifecycle
+
+- (id)init; {
+    return [self initWithInputValue:nil outputValue:nil];
+}
+
+- (id)initWithInputValue:(id)inputValue outputValue:(id)outputValue; {
+    // TODO
+    return nil;
+}
+
+#pragma mark Transformation
+
+- (id)transform:(id)obj; {
+    // TODO
+    return obj;
+}
+
+- (PROTransformation *)reverseTransformation; {
+    // TODO
+    return self;
+}
+
+#pragma mark NSCoding
+
+- (id)initWithCoder:(NSCoder *)coder {
+    // TODO
+    return nil;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+    // TODO
+}
+
+#pragma mark NSCopying
+
+- (id)copyWithZone:(NSZone *)zone {
+    // this object is immutable
+    return self;
+}
+
 @end
