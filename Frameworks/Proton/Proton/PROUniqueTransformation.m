cbaa9d7b51d50f898ab093f2ec466669aa9b8f27
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-13 12:26:12 -0800

    Implemented PROUniqueTransformation

--- Frameworks/Proton/Proton/PROUniqueTransformation.m
+++ Frameworks/Proton/Proton/PROUniqueTransformation.m
@@ -7,6 +7,8 @@
 //
 
 #import <Proton/PROUniqueTransformation.h>
+#import <Proton/EXTNil.h>
+#import <Proton/NSObject+ComparisonAdditions.h>
 
 @implementation PROUniqueTransformation
 
@@ -15,6 +17,11 @@
 @synthesize inputValue = m_inputValue;
 @synthesize outputValue = m_outputValue;
 
+- (PROTransformation *)reverseTransformation; {
+    // just flip our values around
+    return [[PROUniqueTransformation alloc] initWithInputValue:self.outputValue outputValue:self.inputValue];
+}
+
 #pragma mark Lifecycle
 
 - (id)init; {
@@ -22,31 +29,55 @@
 }
 
 - (id)initWithInputValue:(id)inputValue outputValue:(id)outputValue; {
-    // TODO
-    return nil;
+    self = [super init];
+    if (!self)
+        return nil;
+
+    // if both are nil, leave them nil
+    // if one is nil, make it EXTNil
+    // copy non-nil values
+    if (inputValue) {
+        m_inputValue = [inputValue copy];
+
+        if (outputValue) {
+            m_outputValue = [outputValue copy];
+        } else {
+            m_outputValue = [EXTNil null];
+        }
+    } else if (outputValue) {
+        m_inputValue = [EXTNil null];
+        m_outputValue = [outputValue copy];
+    }
+
+    return self;
 }
 
 #pragma mark Transformation
 
 - (id)transform:(id)obj; {
-    // TODO
-    return obj;
-}
+    if (!self.inputValue)
+        return obj;
 
-- (PROTransformation *)reverseTransformation; {
-    // TODO
-    return self;
+    if ([self.inputValue isEqual:obj])
+        return self.outputValue;
+    else
+        return nil;
 }
 
 #pragma mark NSCoding
 
 - (id)initWithCoder:(NSCoder *)coder {
-    // TODO
-    return nil;
+    id inputValue = [coder decodeObjectForKey:@"inputValue"];
+    id outputValue = [coder decodeObjectForKey:@"outputValue"];
+    return [self initWithInputValue:inputValue outputValue:outputValue];
 }
 
 - (void)encodeWithCoder:(NSCoder *)coder {
-    // TODO
+    if (self.inputValue)
+        [coder encodeObject:self.inputValue forKey:@"inputValue"];
+
+    if (self.outputValue)
+        [coder encodeObject:self.outputValue forKey:@"outputValue"];
 }
 
 #pragma mark NSCopying
@@ -54,6 +85,25 @@
 - (id)copyWithZone:(NSZone *)zone {
     // this object is immutable
     return self;
+}
+
+#pragma mark NSObject overrides
+
+- (NSUInteger)hash {
+    return [self.inputValue hash] ^ [self.outputValue hash];
+}
+
+- (BOOL)isEqual:(PROUniqueTransformation *)transformation {
+    if (![transformation isKindOfClass:[PROUniqueTransformation class]])
+        return NO;
+
+    if (!NSEqualObjects(self.inputValue, transformation.inputValue))
+        return NO;
+
+    if (!NSEqualObjects(self.outputValue, transformation.outputValue))
+        return NO;
+
+    return YES;
 }
 
 @end
