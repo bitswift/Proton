4f8feb990b851b82baefa24d29a32f45d2d29671
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-13 19:19:02 -0800

    First draft of the basic interfaces for PROModel and PRODictionaryModel classes. This version does not include automatic setter transformations -- that can come a bit later.

---
+++ Frameworks/Proton/Proton/PRODictionaryModel.m
@@ -0,0 +1,13 @@
+//
+//  PRODictionaryModel.m
+//  Proton
+//
+//  Created by Justin Spahr-Summers on 13.12.11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import <Proton/PRODictionaryModel.h>
+
+@implementation PRODictionaryModel
+
+@end
---
+++ Frameworks/Proton/Proton/PROModel.m
@@ -0,0 +1,13 @@
+//
+//  PROModel.m
+//  Proton
+//
+//  Created by Justin Spahr-Summers on 13.12.11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import <Proton/PROModel.h>
+
+@implementation PROModel
+
+@end
