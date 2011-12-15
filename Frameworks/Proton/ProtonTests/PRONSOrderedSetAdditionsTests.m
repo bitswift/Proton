14d2b65e7b20c80c0aa539a235e89247ab5a9923
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-15 12:30:44 -0600

    Implemented -mapUsingBlock: and -mapWithOptions:usingBlock: for NSOrderedSet

--- Frameworks/Proton/ProtonTests/PRONSOrderedSetAdditionsTests.m
+++ Frameworks/Proton/ProtonTests/PRONSOrderedSetAdditionsTests.m
@@ -11,6 +11,7 @@
 
 @interface PRONSOrderedSetAdditionsTests ()
 - (void)testFilteringWithOptions:(NSEnumerationOptions)opts;
+- (void)testMappingWithOptions:(NSEnumerationOptions)opts;
 @end
 
 @implementation PRONSOrderedSetAdditionsTests
@@ -65,5 +66,67 @@
 
     NSOrderedSet *testOrderedSet = [NSOrderedSet orderedSetWithObjects:@"foo", @"bar", nil];
     STAssertEqualObjects(filteredOrderedSet, testOrderedSet, @"");
+}
+
+- (void)testMapping {
+    [self testMappingWithOptions:0];
+}
+
+- (void)testMappingEmptyOrderedSet {
+    NSOrderedSet *orderedSet = [NSOrderedSet orderedSet];
+
+    NSOrderedSet *mappedOrderedSet = [orderedSet mapUsingBlock:^(NSString *obj){
+        return [obj stringByAppendingString:@"buzz"];
+    }];
+
+    STAssertEqualObjects(mappedOrderedSet, [NSOrderedSet orderedSet], @"");
+}
+
+- (void)testMappingRemovingElements {
+    NSOrderedSet *orderedSet = [NSOrderedSet orderedSetWithObjects:@"foo", @"bar", @"baz", nil];
+
+    NSOrderedSet *mappedOrderedSet = [orderedSet mapUsingBlock:^(NSString *obj){
+        if ([obj hasPrefix:@"b"])
+            return [@"buzz" stringByAppendingString:obj];
+        else
+            return nil;
+    }];
+
+    NSOrderedSet *expectedOrderedSet = [NSOrderedSet orderedSetWithObjects:@"buzzbar", @"buzzbaz", nil];
+    STAssertEqualObjects(mappedOrderedSet, expectedOrderedSet, @"");
+}
+
+- (void)testMappingConcurrently {
+    [self testMappingWithOptions:NSEnumerationConcurrent];
+}
+
+- (void)testMappingReverse {
+    [self testMappingWithOptions:NSEnumerationReverse];
+}
+
+- (void)testMappingWithOptions:(NSEnumerationOptions)opts; {
+    NSOrderedSet *orderedSet = [NSOrderedSet orderedSetWithObjects:@"foo", @"bar", @"baz", @"barbuzz", nil];
+
+    NSOrderedSet *mappedOrderedSet = [orderedSet mapWithOptions:opts usingBlock:^(NSString *obj){
+        // 'barbuzz' should be created twice -- since it's a set, only the first
+        // one should remain in the result
+        if ([obj rangeOfString:@"buzz"].location == NSNotFound)
+            return [obj stringByAppendingString:@"buzz"];
+        else
+            return obj;
+    }];
+
+    NSOrderedSet *expectedOrderedSet;
+
+    if (opts & NSEnumerationReverse) {
+        // the order of this one is not exactly the reverse of the normal case,
+        // since there are two 'barbuzz' entries, and the first one is the one
+        // whose index is used
+        expectedOrderedSet = [NSOrderedSet orderedSetWithObjects:@"barbuzz", @"bazbuzz", @"foobuzz", nil];
+    } else {
+        expectedOrderedSet = [NSOrderedSet orderedSetWithObjects:@"foobuzz", @"barbuzz", @"bazbuzz", nil];
+    }
+
+    STAssertEqualObjects(mappedOrderedSet, expectedOrderedSet, @"");
 }
 @end
