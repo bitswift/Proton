670d0098ae8462cf30ec11241aeb64ab7af5c68c
James Lawton <james.lawton@gmail.com>
2011-12-17 01:59:59 -0800

    Implement PROUniqueIdentifierTests.

---
+++ Frameworks/Proton/ProtonTests/PROUniqueIdentifierTests.m
@@ -0,0 +1,56 @@
+//
+//  PROUniqueIdentifierTests.m
+//  Proton
+//
+//  Created by James Lawton on 12/17/11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import "PROUniqueIdentifierTests.h"
+#import "PROUniqueIdentifier.h"
+
+@implementation PROUniqueIdentifierTests
+
+- (void)testInitialization {
+    PROUniqueIdentifier *uid = [[PROUniqueIdentifier alloc] init];
+    STAssertNotNil(uid, @"");
+}
+
+- (void)testInitializationWithString {
+    NSString *uuidString = @"49DDFC35-8DB7-424D-8BD3-1D7FD8508A58";
+    PROUniqueIdentifier *uid = [[PROUniqueIdentifier alloc] initWithString:uuidString];
+
+    STAssertEqualObjects(uid.stringValue, uuidString, @"");
+}
+
+- (void)testCoding {
+    PROUniqueIdentifier *uid = [[PROUniqueIdentifier alloc] init];
+
+    NSData *encoded = [NSKeyedArchiver archivedDataWithRootObject:uid];
+    PROUniqueIdentifier *decoded = [NSKeyedUnarchiver unarchiveObjectWithData:encoded];
+
+    STAssertEqualObjects(uid, decoded, @"");
+}
+
+- (void)testCopying {
+    PROUniqueIdentifier *uid1 = [[PROUniqueIdentifier alloc] init];
+    PROUniqueIdentifier *uid2 = [uid1 copy];
+    STAssertEqualObjects(uid1, uid2, @"");
+}
+
+- (void)testUniqueness {
+    PROUniqueIdentifier *uid1 = [[PROUniqueIdentifier alloc] init];
+    PROUniqueIdentifier *uid2 = [[PROUniqueIdentifier alloc] init];
+    STAssertFalse([uid1 isEqual:uid2], @"");
+}
+
+- (void)testStringValue {
+    PROUniqueIdentifier *uid1 = [[PROUniqueIdentifier alloc] init];
+    NSString *str = [uid1 stringValue];
+    STAssertNotNil(str, @"");
+
+    PROUniqueIdentifier *uid2 = [[PROUniqueIdentifier alloc] initWithString:str];
+    STAssertEqualObjects(uid1, uid2, @"");
+}
+
+@end
