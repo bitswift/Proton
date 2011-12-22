ded175567b11df7576fd4f719e15ae9b950a3792
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-22 04:56:42 -0800

    Defined an interface for an NSObject extension for block-based KVO

---
+++ Frameworks/Proton/Proton/NSObject+KVOAdditions.m
@@ -0,0 +1,14 @@
+//
+//  NSObject+KVOAdditions.m
+//  Proton
+//
+//  Created by Justin Spahr-Summers on 22.12.11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import <Proton/NSObject+KVOAdditions.h>
+#import <Proton/EXTSafeCategory.h>
+
+@safecategory (NSObject, KVOAdditions)
+
+@end
