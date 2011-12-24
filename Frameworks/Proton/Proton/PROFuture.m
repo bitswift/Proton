2c25ba4a2dd3b37039c9c8c447a5936642c003ce
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-24 02:24:11 -0800

    Defined an interface for PROFuture, a lightweight block-based future

---
+++ Frameworks/Proton/Proton/PROFuture.m
@@ -0,0 +1,13 @@
+//
+//  PROFuture.m
+//  Proton
+//
+//  Created by Justin Spahr-Summers on 24.12.11.
+//  Copyright (c) 2011 Emerald Lark. All rights reserved.
+//
+
+#import <Proton/PROFuture.h>
+
+@implementation PROFuture
+
+@end
