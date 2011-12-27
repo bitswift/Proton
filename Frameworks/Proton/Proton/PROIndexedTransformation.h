2d9ff6fdecfef7f0cc084b5d10bbb1e6a1595047
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-27 15:37:43 -0800

    Changed PROIndexedTransformation interface to support transforming multiple indices simultaneously

--- Frameworks/Proton/Proton/PROIndexedTransformation.h
+++ Frameworks/Proton/Proton/PROIndexedTransformation.h
@@ -9,7 +9,7 @@
 #import <Proton/PROTransformation.h>
 
 /**
- * Describes a transformation to be applied to a specific index of an object.
+ * Describes a transformation to be applied to specific indices of an object.
  */
 @interface PROIndexedTransformation : PROTransformation <NSCoding, NSCopying>
 
@@ -20,44 +20,56 @@
 /**
  * Initializes the receiver to pass through values without modification.
  *
- * Invokes <initWithTransformation:index:> with a `nil` transformation and an
- * index of zero.
+ * Invokes <initWithTransformations:indexes:> with `nil` objects.
  */
 - (id)init;
 
 /**
- * Initializes the receiver to transform a value at a given index using the
- * given transformation.
+ * Initializes the receiver to transform the values at the given indices using
+ * the given transformations.
+ *
+ * `transformations` and `indexes` must be of the same size. If both are `nil`
+ * or empty, the receiver is initialized to pass through all values without
+ * modification.
  *
  * This is the designated initializer.
  *
- * @param transformation A <PROTransformation> to apply.
- * @param index The index at which to apply `transformation`.
+ * @param transformations A <PROTransformation> to apply for each index specified
+ * in `indexes`.
+ * @param indexes The indexes at which to apply the corresponding objects in
+ * `transformations`.
  */
-- (id)initWithTransformation:(PROTransformation *)transformation index:(NSUInteger)index;
+- (id)initWithTransformations:(NSArray *)transformations indexes:(NSIndexSet *)indexes;
 
 /**
  * @name Transformation
  */
 
 /**
- * The transformation performed by the receiver upon the value at
- * <index>.
+ * The transformations performed by the receiver for each index specified in
+ * <indexes>.
  *
- * If this value is `nil`, all objects are passed through without modification.
+ * If this array is empty, all objects are passed through without modification.
  */
-@property (nonatomic, copy, readonly) PROTransformation *transformation;
+@property (nonatomic, copy, readonly) NSArray *transformations;
 
 /**
- * The index associated with the value which will be transformed by the
- * receiver's <transformation>.
+ * The indexes associated with the values which will be transformed by the
+ * receiver's <transformations>.
+ *
+ * Each index in this set corresponds to one object from <transformations>,
+ * which is the transformation to apply at that index.
+ *
+ * If this index set is `nil`, all objects are passed through without
+ * modification.
  */
-@property (nonatomic, readonly) NSUInteger index;
+@property (nonatomic, copy, readonly) NSIndexSet *indexes;
 
 /**
- * Attempts to transform the given array. If the <index> is out of bounds for the given array, `nil` is returned.
+ * Attempts to transform the given array. If <indexes> is out of bounds for the
+ * given array, `nil` is returned.
  *
- * @param array The array in which to transform the value at <index>.
+ * @param array The array in which to transform objects.
  */
 - (id)transform:(id)array;
 
