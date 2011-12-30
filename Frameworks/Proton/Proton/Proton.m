670a58f035baa813a051f40d8db82cccc4804414
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-12-29 20:31:53 -0800

    When logging to the TTY, include function (or method) name and line number. When logging to ASL, include short filename (no path or extension) and line number.
    Reviewed by Josh.

--- Frameworks/Proton/Proton/Proton.m
+++ Frameworks/Proton/Proton/Proton.m
@@ -16,10 +16,40 @@
     int ddLogLevel = LOG_LEVEL_ERROR;
 #endif
 
+/*
+ * Formats TTY (Xcode console) logs with the caller's function name and line
+ * number.
+ */
+@interface PROTTYLogFormatter : NSObject <DDLogFormatter>
+@end
+
+/*
+ * Formats ASL (system console) logs with the caller's file and line number, to
+ * reveal fewer implementation details than <PROTTYLogFormatter>.
+ */
+@interface PROASLLogFormatter : NSObject <DDLogFormatter>
+@end
+
 // automatically sets up an ASL and TTY logger at startup
 __attribute__((constructor))
 static void initializeLogging (void) {
+    [DDASLLogger sharedInstance].logFormatter = [[PROASLLogFormatter alloc] init];
     [DDLog addLogger:[DDASLLogger sharedInstance]];
+
+    [DDTTYLogger sharedInstance].logFormatter = [[PROTTYLogFormatter alloc] init];
     [DDLog addLogger:[DDTTYLogger sharedInstance]];
 }
+
+@implementation PROTTYLogFormatter
+- (NSString *)formatLogMessage:(DDLogMessage *)logMessage; {
+    return [[NSString alloc] initWithFormat:@"%s:%i: %@", logMessage->function, logMessage->lineNumber, logMessage->logMsg];
+}
+@end
+
+@implementation PROASLLogFormatter
+- (NSString *)formatLogMessage:(DDLogMessage *)logMessage; {
+    // use short filename instead of function
+    return [[NSString alloc] initWithFormat:@"%@:%i: %@", [logMessage fileName], logMessage->lineNumber, logMessage->logMsg];
+}
+@end
 
