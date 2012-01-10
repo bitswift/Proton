75a3aaaf3beb4f607149df43342128fcf8da40c0
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-01-10 02:02:37 -0800

    Implemented PROKeyForClass(), which works like PROKeyForObject(), but with class names (for cases where there's no instance to refer to)

--- Frameworks/Proton/Proton/PROKeyValueCodingMacros.h
+++ Frameworks/Proton/Proton/PROKeyValueCodingMacros.h
@@ -24,3 +24,18 @@
  */
 #define PROKeyForObject(OBJECT, KEY) \
     ((void)(NO && ((void)OBJECT.KEY, NO)), @ # KEY )
+
+/**
+ * Identical to <PROKeyForObject>, but must be used when trying to look up a key
+ * path with only a class object.
+ *
+ * @param CLASS The class whose instances should contain property `KEY`, as
+ * a symbol.
+ * @param KEY The name of a property on instances of `CLASS`, as a symbol. You
+ * may also name descendant properties (e.g., `object.dictionary.key`) here, in
+ * which case the path will be checked for validity. Key-value path operators
+ * (those beginning with `@`) are not supported.
+ */
+#define PROKeyForClass(CLASS, KEY) \
+    ((void)(NO && ^(CLASS *obj){ (void)obj.KEY; }), @ # KEY )
+
