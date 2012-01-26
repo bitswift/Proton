0fb3d7b22eacc4abd9f4453073de6b349c58ba00
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-01-25 18:14:09 -0800

    Revert "Added -[NSUndoManager registerGroupWithActionName:block:undoBlock:], for block-based undo (with some restrictions)"
    This reverts commit 72bff82409746387451bdfa30611109303d383d2.

--- Proton/Proton.h
+++ Proton/Proton.h
@@ -20,7 +20,6 @@
 #import <Proton/NSObject+PROKeyValueObserverAdditions.h>
 #import <Proton/NSOrderedSet+HigherOrderAdditions.h>
 #import <Proton/NSSet+HigherOrderAdditions.h>
-#import <Proton/NSUndoManager+RegistrationAdditions.h>
 #import <Proton/NSUndoManager+UndoStackAdditions.h>
 #import <Proton/PROAssert.h>
 #import <Proton/PROBacktraceFunctions.h>
--- ProtonTests/PRONSUndoManagerAdditionsTests.m
+++ ProtonTests/PRONSUndoManagerAdditionsTests.m
@@ -6,7 +6,6 @@
 //  Copyright (c) 2012 Bitswift. All rights reserved.
 //
 
-#import "NSUndoManager+RegistrationAdditions.h"
 #import "NSUndoManager+UndoStackAdditions.h"
 
 SpecBegin(NSUndoManagerAdditions)
@@ -22,89 +21,20 @@ SpecBegin(NSUndoManagerAdditions)
         expect(undoManager.canRedo).toBeFalsy();
     });
 
-    it(@"should undo nested group without registering redo", ^{
-        [undoManager beginUndoGrouping];
-        [undoManager endUndoGrouping];
+    describe(@"UndoStackAdditions", ^{
+        it(@"should undo nested group without registering redo", ^{
+            [undoManager beginUndoGrouping];
+            [undoManager endUndoGrouping];
 
-        expect(undoManager.canUndo).toBeTruthy();
-        expect(undoManager.canRedo).toBeFalsy();
-
-        [undoManager undoNestedGroupingWithoutRegisteringRedo];
-
-        expect(undoManager.canUndo).toBeFalsy();
-        expect(undoManager.canRedo).toBeFalsy();
-    });
-
-    describe(@"registration additions", ^{
-        __block NSInteger changeCount;
-
-        __block BOOL (^incrementBlock)(void);
-        __block BOOL (^failingIncrementBlock)(void);
-        __block void (^decrementBlock)(void);
-
-        before(^{
-            changeCount = 0;
-
-            incrementBlock = ^{
-                ++changeCount;
-                return YES;
-            };
-
-            failingIncrementBlock = ^{
-                ++changeCount;
-                return NO;
-            };
-
-            decrementBlock = ^{
-                --changeCount;
-            };
-        });
-
-        it(@"should execute block", ^{
-            BOOL success = [undoManager registerGroupWithActionName:nil block:incrementBlock undoBlock:decrementBlock];
-            
-            expect(success).toBeTruthy();
-            expect(changeCount).toEqual(1);
-        });
-
-        it(@"should register undo", ^{
-            [undoManager registerGroupWithActionName:nil block:incrementBlock undoBlock:decrementBlock];
             expect(undoManager.canUndo).toBeTruthy();
+            expect(undoManager.canRedo).toBeFalsy();
 
-            [undoManager undo];
-            expect(changeCount).toEqual(0);
-        });
+            [undoManager undoNestedGroupingWithoutRegisteringRedo];
 
-        it(@"should register redo", ^{
-            [undoManager registerGroupWithActionName:nil block:incrementBlock undoBlock:decrementBlock];
-
-            [undoManager undo];
-            expect(undoManager.canRedo).toBeTruthy();
-            
-            [undoManager redo];
-            expect(changeCount).toEqual(1);
-        });
-
-        it(@"should not register undo on failure", ^{
-            BOOL success = [undoManager registerGroupWithActionName:nil block:failingIncrementBlock undoBlock:decrementBlock];
-
-            expect(success).toBeFalsy();
             expect(undoManager.canUndo).toBeFalsy();
             expect(undoManager.canRedo).toBeFalsy();
-            expect(changeCount).toEqual(0);
-        });
-
-        it(@"should set action name", ^{
-            [undoManager
-                registerGroupWithActionName:@"foobar"
-                block:^{
-                    expect(undoManager.undoActionName).toEqual(@"foobar");
-                    return YES;
-                }
-
-                undoBlock:^{}
-            ];
         });
     });
     
 SpecEnd
+
