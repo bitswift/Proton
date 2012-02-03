a5f7f763b1f360194caa1db2d26455371ab4c49c
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-02-03 09:46:01 -0800

    Interface for a PROKeyValueCodingProxy class

---
+++ Proton/PROKeyValueCodingProxy.m
@@ -0,0 +1,34 @@
+//
+//  PROKeyValueCodingProxy.m
+//  Proton
+//
+//  Created by Justin Spahr-Summers on 03.02.12.
+//  Copyright (c) 2012 Bitswift. All rights reserved.
+//
+
+#import "PROKeyValueCodingProxy.h"
+
+@implementation PROKeyValueCodingProxy
+
+#pragma mark Properties
+
+@synthesize keyPath = m_keyPath;
+@synthesize setValueForKeyPathBlock = m_setValueForKeyPathBlock;
+@synthesize valueForKeyPathBlock = m_valueForKeyPathBlock;
+@synthesize mutableArrayValueForKeyPathBlock = m_mutableArrayValueForKeyPathBlock;
+
+#pragma mark Initialization
+
+- (id)init; {
+    return nil;
+}
+
+- (id)initWithKeyPath:(NSString *)keyPath; {
+    return nil;
+}
+
+#pragma mark NSKeyValueCoding
+
+#pragma mark NSObject overrides
+
+@end
