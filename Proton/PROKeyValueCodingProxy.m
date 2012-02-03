3a89613b28030fe89b1cb1a18c287a38de777424
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-02-03 13:00:04 -0800

    Implemented PROKeyValueCodingProxy

--- Proton/PROKeyValueCodingProxy.m
+++ Proton/PROKeyValueCodingProxy.m
@@ -8,6 +8,10 @@
 
 #import "PROKeyValueCodingProxy.h"
 
+@interface PROKeyValueCodingProxy ()
+@property (nonatomic, copy, readwrite) NSString *keyPath;
+@end
+
 @implementation PROKeyValueCodingProxy
 
 #pragma mark Properties
@@ -20,21 +24,67 @@
 #pragma mark Initialization
 
 - (id)init; {
-    return nil;
+    return [self initWithKeyPath:nil];
 }
 
 - (id)initWithKeyPath:(NSString *)keyPath; {
-    return nil;
+    self = [super init];
+    if (!self)
+        return nil;
+
+    self.keyPath = keyPath;
+    return self;
 }
 
 #pragma mark Nested Proxies
 
 - (PROKeyValueCodingProxy *)proxyForKeyPath:(NSString *)keyPath; {
-    return nil;
+    PROKeyValueCodingProxy *proxy = [[[self class] alloc] initWithKeyPath:keyPath];
+
+    proxy.setValueForKeyPathBlock = self.setValueForKeyPathBlock;
+    proxy.valueForKeyPathBlock = self.valueForKeyPathBlock;
+    proxy.mutableArrayValueForKeyPathBlock = self.mutableArrayValueForKeyPathBlock;
+
+    return proxy;
 }
 
 #pragma mark NSKeyValueCoding
 
-#pragma mark NSObject overrides
++ (BOOL)accessInstanceVariablesDirectly {
+    return NO;
+}
+
+- (NSMutableArray *)mutableArrayValueForKey:(NSString *)key {
+    return [self mutableArrayValueForKeyPath:key];
+}
+
+- (NSMutableArray *)mutableArrayValueForKeyPath:(NSString *)keyPath {
+    if (self.mutableArrayValueForKeyPathBlock)
+        return self.mutableArrayValueForKeyPathBlock(keyPath);
+    else
+        return [super mutableArrayValueForKeyPath:keyPath];
+}
+
+- (void)setValue:(id)value forKey:(NSString *)key {
+    [self setValue:value forKeyPath:key];
+}
+
+- (void)setValue:(id)value forKeyPath:(NSString *)keyPath {
+    if (self.setValueForKeyPathBlock)
+        self.setValueForKeyPathBlock(value, keyPath);
+    else
+        [super setValue:value forKeyPath:keyPath];
+}
+
+- (id)valueForKey:(NSString *)key {
+    return [self valueForKeyPath:key];
+}
+
+- (id)valueForKeyPath:(NSString *)keyPath {
+    if (self.valueForKeyPathBlock)
+        return self.valueForKeyPathBlock(keyPath);
+    else
+        return [super valueForKeyPath:keyPath];
+}
 
 @end
