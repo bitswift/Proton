6ca5ffaa0f108562443f935ee8ce1bfafcf4adc0
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-02-03 10:17:19 -0800

    Tests for PROKeyValueCodingProxy

---
+++ ProtonTests/PROKeyValueCodingProxyTests.m
@@ -0,0 +1,15 @@
+//
+//  PROKeyValueCodingProxyTests.m
+//  Proton
+//
+//  Created by Justin Spahr-Summers on 03.02.12.
+//  Copyright (c) 2012 Bitswift. All rights reserved.
+//
+
+#import <Proton/PROKeyValueCodingProxy.h>
+
+SpecBegin(PROKeyValueCodingProxy)
+
+
+
+SpecEnd
