b6f2a244d180e2ce83ba545dfd864382e3e57d20
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-02-13 22:43:03 -0800

    Combined PROModelController and PROMutableModel functionality into a new, more powerful PROMutableModel interface

--- Proton/PROMutableModel.h
+++ Proton/PROMutableModel.h
@@ -7,38 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "PROKeyedObject.h"
 
 @class PROModel;
-@class PROModelController;
-
-/**
- * A notification posted when a <PROMutableModel> has updated to the latest
- * <[PROModelController model]> from its model controller, and then successfully
- * reapplied its own changes on top.
- *
- * This notification may be posted on a background thread.
- */
-extern NSString * const PROMutableModelDidRebaseFromModelControllerNotification;
-
-/**
- * A notification posted when a <PROMutableModel> attempted to update to the
- * latest <[PROModelController model]> from its model controller, but failed to
- * apply its own changes on top.
- *
- * The <PROMutableModel> is left unchanged.
- *
- * The user info dictionary for this notification will contain
- * a <PROMutableModelRebaseErrorKey>.
- *
- * This notification may be posted on a background thread.
- */
-extern NSString * const PROMutableModelRebaseFromModelControllerFailedNotification;
-
-/**
- * Notification user info key associated with the `NSError` that occurred when
- * attempting to rebase a <PROMutableModel>.
- */
-extern NSString * const PROMutableModelRebaseErrorKey;
+@class PROTransformationLogEntry;
 
 // the documentation below uses a different comment style so that code blocks
 // are properly included in the generated documentation
@@ -66,50 +38,138 @@ typed to <PROModel>:
 @required
 
 /**
- * @name Model Controller
- */
-
-/**
- * The model controller specified at initialization, or `nil` if the receiver
- * was not initialized with a model controller.
- */
-@property (nonatomic, strong, readonly) PROModelController *modelController;
-
-/**
  * @name Copying
  */
 
 /**
- * Returns a <PROModel> object representing the current state of the receiver.
+ * Atomically copies the receiver into an immutable <PROModel> object.
+ *
+ * The same object may be returned across multiple calls to this method if the
+ * data has not changed in the interim.
  *
  * @param zone Unused.
  */
 - (id)copyWithZone:(NSZone *)zone;
 
 /**
- * Returns a new <PROMutableModel>, initialized to the same state as the
- * receiver.
+ * Atomically copies the receiver into a new <PROMutableModel>.
  *
  * @param zone Unused.
  */
 - (id)mutableCopyWithZone:(NSZone *)zone;
 
+#if 0
 /**
- * @name Saving Changes
+ * @name Transactions
  */
 
 /**
- * Attempts to save changes made on the receiver back to its <modelController>.
- * Returns `YES` upon success. If the save is not successful, `NO` is returned
- * and `error` is set to the error that occurred.
+ * Atomically executes a transaction, returning `YES` upon success. If the
+ * transaction fails, `NO` is returned and `error` is set to the error that
+ * occurred.
  *
- * If the receiver does not have a <modelController>, this method returns `YES`
- * without doing anything.
+ * Transactions can be used to group modifications to the receiver (and/or other
+ * models in its hierarchy) and perform them all atomically, thus avoiding race
+ * conditions where other threads might make modifications during the same
+ * period of time.
  *
- * @param error If not `NULL`, and this method returns `NO`, this may be set to
- * the error that occurred.
+ * If any mutation performed during the transaction would result in an invalid
+ * model object, the rest of the transaction block executes, but `NO` is
+ * returned, and all of the models are left untouched.
+ *
+ * If the transaction succeeds, a <PROMultipleTransformation> is added to an
+ * internal "transformation log," which can be used to replay transformations in
+ * reverse and retrieve an older copy of the receiver. See
+ * <transformationLogEntry> for more information.
+ *
+ * @param error If not `NULL`, this is set to any error that occurs. This
+ * argument will only be set if the method returns `NO`.
+ * @param block A block containing modifications to apply to the receiver or
+ * other models in its same hierarchy.
+ *
+ * @warning **Important:** This method briefly synchronizes with the main thread
+ * before committing the transformation, to make strong guarantees about data
+ * consistency on the main thread. This means that invoking this method from
+ * a background thread may block the caller for an indeterminate period of time.
  */
-- (BOOL)save:(NSError **)error;
+- (BOOL)performTransactionWithError:(NSError **)error usingBlock:(void (^)(void))block;
+#endif
+
+/**
+ * @name Applying Transformations
+ */
+
+/**
+ * Atomically applies the given transformation to the receiver, returning `YES`
+ * upon success. If the transformation fails, `NO` is returned and `error` is
+ * set to the error that occurred.
+ *
+ * If the transformation succeeds, it is added to an internal "transformation
+ * log," which can be used to replay transformations in reverse and retrieve an
+ * older copy of the receiver. See <transformationLogEntry> for more
+ * information.
+ *
+ * @param transformation The transformation to attempt to apply.
+ * @param error If not `NULL`, this is set to any error that occurs. This
+ * argument will only be set if the method returns `NO`.
+ *
+ * @warning **Important:** This method briefly synchronizes with the main thread
+ * before committing the transformation, to make strong guarantees about data
+ * consistency on the main thread. This means that invoking this method from
+ * a background thread may block the caller for an indeterminate period of time.
+ */
+- (BOOL)applyTransformation:(PROTransformation *)transformation error:(NSError **)error;
+
+/**
+ * @name Transformation Log
+ */
+
+/**
+ * The maximum number of <PROTransformation> instances to include in the
+ * transformation log when the receiver is archived, or zero to disable limiting
+ * of the archived log.
+ *
+ * There is intentionally no way to limit a transformation log's in-memory size
+ * limit, as doing so would introduce more problems than it would solve.
+ *
+ * The default value for this property is 50.
+ */
+@property (assign) NSUInteger archivedTransformationLogLimit;
+
+/**
+ * Retrieves the latest transformation log entry.
+ *
+ * The transformation log entry can later be passed to
+ * <modelWithTransformationLogEntry:> or <restoreTransformationLogEntry:> to
+ * retrieve the model as it existed when this method was invoked.
+ */
+@property (nonatomic, copy, readonly) PROTransformationLogEntry *transformationLogEntry;
+
+/**
+ * Returns the version of the receiver that corresponds to the given
+ * transformation log entry, or `nil` if the entry no longer exists in the log.
+ *
+ * The given log entry may no longer exist if the transformation was archived
+ * and trimmed to stay within the <archivedTransformationLogLimit>.
+ *
+ * @param transformationLogEntry An object previously returned from
+ * <transformationLogEntry>.
+ */
+- (id)modelWithTransformationLogEntry:(PROTransformationLogEntry *)transformationLogEntry;
+
+/**
+ * Restores the version of the receiver that corresponds to the given log entry.
+ * Returns `YES` on success, or `NO` if the entry no longer exists in the log.
+ *
+ * This method is better suited to undo and redo than
+ * <modelWithTransformationLogEntry:>, since this method can rollback or update
+ * a mutable model in-place, and without any duplication of data.
+ *
+ * @param transformationLogEntry An object previously returned from
+ * <transformationLogEntry>.
+ */
+- (BOOL)restoreTransformationLogEntry:(PROModelControllerTransformationLogEntry *)transformationLogEntry;
+
 @end
 
 /**
@@ -119,13 +179,15 @@ usage patterns easier.
 
 This class will automatically provide setters for any properties that exist
 on the underlying <PROModel>, and will support key-value coding and key-value
-observing on all of the model object's properties. Note that other <PROModel>
-instances are not automatically made mutable when using this class (though
-any collection containing them may be).
+observing on all of the model object's properties.
+
+TODO: describe transactions and the threading model
 
 Internally, a record of <PROTransformation> objects is kept that describes
-the changes being made to the <PROModel>. When <save:> is invoked, it will
-attempt to propagate those changes back to a <PROModelController>.
+the changes being made to the <PROModel>. Entries from this log can be retrieved
+with <[PROMutableModel transformationLogEntry]> and later restored using
+<[PROMutableModel modelWithTransformationLogEntry:]> or <[PROMutableModel
+restoreTransformationLogEntry:]>.
 
 If an instance of this class receives a message it does not understand, the
 message is automatically forwarded to the underlying <PROModel> object.
@@ -150,46 +212,27 @@ provide no implementation, like so:
     @synthesize someString = m_someString;
     @end
 
-Changes to instances of this class are performed atomically. Note, however,
-that this is not composable. This means that changes to multiple
-properties at a time may not be committed atomically with respect to other
-threads.
-
  */
-@interface PROMutableModel : NSObject <PROMutableModel>
+@interface PROMutableModel : NSObject <PROKeyedObject, PROMutableModel>
 
 /**
  * @name Initialization
  */
 
 /**
- * Initializes the receiver to proxy the given model object. Returns `nil` if
- * the given model is `nil`.
+ * Initializes the receiver with the given model object. Returns `nil` if the
+ * given model is `nil`.
  *
- * Any changes made to the receiver will not be propagated back to any model
- * controller. Invoking <save:> will have no effect.
- *
- * @param model The model object that the receiver should proxy.
+ * @param model The model object that the receiver should be a mutable copy of.
  */
 - (id)initWithModel:(PROModel *)model;
 
 /**
- * Initializes the receiver to proxy the model object of the given controller.
- * Returns `nil` if the given model is `nil`.
+ * Initializes the receiver as a copy of the given mutable model object. Returns
+ * `nil` if the given object is `nil`.
  *
- * Invoking <save:> after making changes to the receiver will attempt to
- * propagate those changes back to the model controller.
- *
- * Initializing the mutable model in this way will automatically set it up to
- * observe the <[PROModelController model]> of the given controller. When the
- * model controller replaces its model, the receiver will attempt to "rebase"
- * onto it, by updating its underlying model and reapplying any changes on top.
- * `PROMutableModelDidRebaseFromModelControllerNotification` or
- * `PROMutableModelRebaseFromModelControllerFailedNotification` will be posted
- * if the automatic rebasing succeeded or failed, respectively.
- *
- * @param modelController The model controller that owns the model which should
- * be proxied.
+ * @param model The model object that the receiver should be a mutable copy of.
  */
-- (id)initWithModelController:(PROModelController *)modelController;
+- (id)initWithMutableModel:(id<PROMutableModel> *)model;
+
 @end
