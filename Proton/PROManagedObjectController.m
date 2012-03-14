cbbcef1d97aeb74bc35f18c9f6d3cbf70fc70dc0
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-03-14 15:13:59 -0700

    Refactored PROManagedObjectController to invoke -commitEditing when editing completes normally (but not when discarding)

--- Proton/PROManagedObjectController.m
+++ Proton/PROManagedObjectController.m
@@ -24,6 +24,7 @@
         unsigned rollbackOnDiscardEditing:1;
         unsigned hasOpenUndoGroup:1;
         unsigned editing:1;
+        unsigned finishingEdit:1;
     } m_flags;
 }
 
@@ -31,6 +32,11 @@
  * Whether the receiver currently has an undo group open with its <undoManager>.
  */
 @property (nonatomic, assign) BOOL hasOpenUndoGroup;
+
+/**
+ * Whether the receiver is currently in the process of discarding edits.
+ */
+@property (nonatomic, getter = isFinishingEdit) BOOL finishingEdit;
 
 /**
  * Implements `-observationInfo` and `-setObservationInfo:` for improved
@@ -93,6 +99,14 @@
     m_flags.hasOpenUndoGroup = value;
 }
 
+- (BOOL)isFinishingEdit {
+    return m_flags.finishingEdit;
+}
+
+- (void)setFinishingEdit:(BOOL)value {
+    m_flags.finishingEdit = value;
+}
+
 - (BOOL)isEditing {
     return m_flags.editing;
 }
@@ -111,6 +125,12 @@
 
         [self.parentController objectDidBeginEditing:self];
     } else if (wasEditing && !self.editing) {
+        if (!self.finishingEdit) {
+            // invoke commitEditing, to save our context (if applicable) and
+            // walk through any subclass logic
+            [self commitEditing];
+        }
+
         if (self.hasOpenUndoGroup) {
             if (PROAssert(self.undoManager.groupingLevel > 0, @"%@ has an open undo group, but undo manager %@ does not", self, self.undoManager)) {
                 [self.undoManager endUndoGrouping];
@@ -210,6 +230,14 @@
     if (!self.editing)
         return;
 
+    if (!PROAssert(!self.finishingEdit, @"%s should not be invoked while finishing up another edit", __func__))
+        return;
+
+    self.finishingEdit = YES;
+    @onExit {
+        self.finishingEdit = NO;
+    };
+
     [self.currentEditors enumerateObjectsUsingBlock:^(id editor, BOOL *stop){
         if (PROAssert([editor respondsToSelector:@selector(discardEditing)], @"%@ does not implement <NSEditor>", editor))
             [editor discardEditing];
@@ -288,20 +316,25 @@
 - (void)commitEditingAndPerform:(void (^)(BOOL commitSuccessful, NSError *error, id failedEditor))block; {
     NSParameterAssert(block != nil);
 
-    if (!self.editing) {
-        block(YES, nil, nil);
+    if (!PROAssert(!self.finishingEdit, @"%s should not be invoked while finishing up another edit", __func__))
         return;
-    }
 
-    for (id editor in self.currentEditors) {
-        NSError *error = nil;
-        if (![self commitEditor:editor error:&error]) {
-            block(NO, error, editor);
-            return;
+    self.finishingEdit = YES;
+    @onExit {
+        self.finishingEdit = NO;
+    };
+
+    if (self.editing) {
+        for (id editor in self.currentEditors) {
+            NSError *error = nil;
+            if (![self commitEditor:editor error:&error]) {
+                block(NO, error, editor);
+                return;
+            }
         }
     }
 
-    if (self.saveOnCommitEditing && self.managedObjectContext) {
+    if (self.saveOnCommitEditing && self.managedObjectContext.hasChanges) {
         NSError *error = nil;
         if (![self.managedObjectContext save:&error]) {
             block(NO, error, nil);
